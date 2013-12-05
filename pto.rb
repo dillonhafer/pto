ff2703b98c722dce6b3432cf418bfc2550ffca45
Dillon Hafer <dh@dillonhafer.com>
2013-12-05 16:15:08 -0500

    Removes 2013 dates

--- pto.rb
+++ pto.rb
@@ -9,12 +9,6 @@ employees << Employee.new({
   start_date: Date.parse('2013-10-01'),
   task_name: 'Dillon PTO 2014'
 })
-employees << Employee.new({
-  user_id: 606,
-  task_id: 9944,
-  start_date: Date.parse('2012-10-01'),
-  task_name: 'Dillon PTO 2013'
-})
 
 # Seth
 employees << Employee.new({
@@ -23,14 +17,6 @@ employees << Employee.new({
   start_date: Date.parse('2013-07-01'),
   task_name: 'Seth PTO 2014'
 })
-employees << Employee.new({
-  user_id: 618,
-  task_id: 9946,
-  start_date: Date.parse('2013-07-01'),
-  task_name: 'Seth PTO 2013'
-})
-
-# Dani
 
 employees.each do |employee| 
   employee.update_time
