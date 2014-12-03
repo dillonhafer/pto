e9a44bc372f9ccb7ecf157462749c3049487e4c5
Dillon Hafer <dh@dillonhafer.com>
2014-12-03 17:25:36 -0500

    Adds naive hour by year

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,10 +5,35 @@ class Person < ActiveRecord::Base
     alloted_hours - hours_used
   end
 
-  def hours_earned_in(year)
-    today = Date.new(year.to_i,1,1)
-    start = start_date 
-    (today.year * 12 + today.month) - (start.year * 12 + start.month)
+  def hours_earned
+    hour_groups = []
+    (years_since_start+1).times do |i|
+      hours = if years_since_start == i
+        hours_this_year
+      else
+        144
+      end
+
+      hour_groups << OpenStruct.new(year: start_date.year+1+i, hours: hours)
+    end
+    hour_groups
+  end
+
+  def hours_in_year(year)
+    
+  end
+
+  def hours_this_year
+    (months_since_start % 12) * 12
+  end
+
+  def years_since_start 
+    years = Date.today.year - start_date.year
+    years -= 1 if (
+         start_date.month >  Date.today.month or 
+        (start_date.month >= Date.today.month and start_date.day > Date.today.day)
+    )
+    years
   end
 
   def hours_used
