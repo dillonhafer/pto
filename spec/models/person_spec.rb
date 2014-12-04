fe6837780344b883ce8ad6b58f101364881bbb3f
Dillon Hafer <dh@dillonhafer.com>
2014-12-04 16:12:35 -0500

    Finish person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -11,4 +11,36 @@ describe Person do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:start_date) }
   end
+
+  context "based on 12 hours per month for 2 months" do
+    before(:each) do
+      person.start_date = 2.months.ago        
+    end
+
+    describe "#hours_remaining" do
+      it "returns #total_alloted_hours if there are no expenses" do
+        expect(person.hours_remaining).to eq(person.total_alloted_hours.to_f)
+      end
+
+      it "is less than #total_alloted_hours if there are expenses" do
+        FactoryGirl.create(:pto_expense,
+                            person: person,
+                            expense_date: Date.today,
+                            hours: rand(8..16))
+
+        hours_used = person.pto_expenses.sum(:hours)
+        expect(person.hours_remaining.to_i).to be <= person.total_alloted_hours-hours_used
+      end
+    end
+
+    describe "#total_alloted_hours" do
+      it "returns 24 hours earned" do
+        person.total_alloted_hours.should eq(24)
+      end
+
+      it "doesn't return more than 24 hours earned" do
+        expect(person.total_alloted_hours).to be <= 24
+      end
+    end    
+  end
 end
