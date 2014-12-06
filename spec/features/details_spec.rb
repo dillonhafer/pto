083c2ce212b196c68d8267ee460d700af8c4a0b1
Dillon Hafer <dh@dillonhafer.com>
2014-12-06 00:48:30 -0500

    Adds specific text in spec

--- spec/features/details_spec.rb
+++ spec/features/details_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 feature "seeing details" do
   background do
-    FactoryGirl.create(:person)
+    FactoryGirl.create(:person, start_date: 2.months.ago)
   end
 
   scenario "viewing details" do
     visit root_path
     click_link('details')
-    expect(page).to have_content 'All Time'
+    expect(page).to have_content 'All Time - 24 hours'
   end
 end
